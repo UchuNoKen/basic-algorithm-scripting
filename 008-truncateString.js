0003682ece7a5a0d8320e62c9c408e2b4eff7373
UchuNoKen <xen.o.fite@gmail.com>
2019-04-03 14:20:15 -0400

    truncate string

--- 008-truncateString.js
+++ 008-truncateString.js
@@ -63,7 +63,8 @@ function truncateString(str, num) {
   if (str.length <= num) {
     return str;
   } else {
-    return str.slice(0, num > 3 ? num - 3 : num) + "...";
+    // had to change 3 to a number greater than 8 to work
+    return str.slice(0, num > 3 ? num - 11 : num) + "...";
   }
 }
 
